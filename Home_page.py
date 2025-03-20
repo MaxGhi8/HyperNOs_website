20ff09de3ed2c966dd4e1904a61b61cd35540fa3
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-03-20 16:56:00 +0100

    initialize webpage files

---
+++ Home_page.py
@@ -0,0 +1,137 @@
+import streamlit as st
+
+
+# Main page content
+def main():
+
+    # Title
+    st.title(
+        "HyperNOs: Automated and Parallel Library for Neural Operators Research",
+        anchor=False,
+    )
+    # Create a container with custom CSS
+    st.markdown(
+        """
+        <style>
+        .profile-name {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 15px;
+            font-size: 1.2em;
+            font-weight: bold;
+            color: var(--text-color);
+        }
+        .profile-title {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 5px;
+            font-size: 0.9em;
+            color: var(--text-color);
+        }
+        .profile-title-secondary {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            top: -15px;
+            margin-bottom: -10px;
+            font-size: 0.9em;
+            color: var(--text-color);
+        }
+        .email-button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+        .email-button a {
+            background-color: #0066cc;
+            color: white;
+            padding: 8px 16px;
+            border-radius: 4px;
+            text-decoration: none;
+            font-size: 0.9em;
+            transition: background-color 0.3s;
+        }
+        .email-button a:hover {
+            background-color: #0052a3;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    # Create columns for the cards
+    col1, col2, col3 = st.columns([1, 1, 1], border=False)
+
+    with col2:
+        with st.container():
+
+            cols = st.columns([1, 5, 1])
+            with cols[1]:
+                st.image(
+                    "massimiliano_ghiotto.jpg", width=120, use_container_width=True
+                )
+
+            st.markdown(
+                "<p class='profile-name'>Massimiliano Ghiotto</p>",
+                unsafe_allow_html=True,
+            )
+            st.markdown(
+                "<p class='profile-title'>University of Pavia</p>",
+                unsafe_allow_html=True,
+            )
+            st.markdown(
+                """
+                <div class="email-button">
+                    <a href="mailto:massimiliano.ghiotto01@universitadipavia.it">
+                        <span class="email-emoji">🖂</span> Contact Me
+                    </a>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+
+    # Description
+    st.header("Project Description", anchor=False)
+    # st.markdown(
+    #     """
+    #     This web application serves as an interactive visualization tool for comparing and analyzing different Ordinary Differential Equation (ODE) models commonly used in cardiac electrophysiology.
+    #     The featured models include the Hodgkin-Huxley (HH) model, which describes action potential generation in neurons,
+    #     the FitzHugh-Nagumno (FHN) model that is a simplification with two variables of the original HH model,
+    #     and the O'Hara-Rudy (ORD) model, which simulates with 41 variables the human ventricular cell behavior.
+
+    #     For each mathematical model, the application provides comprehensive visualizations across multiple pages. Users can examine:
+
+    #     - Input functions that drive the model simulations.
+    #     - True solutions derived from analytical methods or high-precision numerical calculations, compared with approximate solutions with Fourier Neural Operator (FNO) models.
+    #     - Error analysis showing the absolute value of the difference between true and approximate solutions, helping users understand the accuracy and limitations of different neural operator models.
+
+    #     These visualizations help researchers, students, and practitioners better understand the behavior of these complex biological systems and evaluate the effectiveness of different neural operator models.
+    #     Moreover, for the OHara-Rudy model, we have 41 different and coupled variables with different behaviors, which makes very complex to analyze the behavior all in once inside an article.
+    #     For these reasons, we have created this web application to help the reader to understand the behavior of the model in a more interactive and comprehensive way.
+    # """
+    # )
+
+    # FNO video
+    # st.header("One dimensional Fourier Neural Operator visualization", anchor=False)
+    # st.markdown(
+    #     """
+    #     In this video, we show the architecture of the Fourier Neural Operator for the one-dimensional case, that is the case of interest for our work.
+    # """
+    # )
+    # st.video("FNO_architecture_1d.mp4", loop=True, autoplay=True)
+
+
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="HyperNOs",
+        page_icon=":moyai:",
+        menu_items={
+            "Report a bug": "https://github.com/MaxGhi8/HyperNOs_website/issues",
+            # "About": "sium",
+        },
+    )
+    main()
---
+++ utilities.py
@@ -0,0 +1,205 @@
+import numpy as np
+import plotly.graph_objects as go
+import streamlit as st
+from scipy.io import loadmat
+
+
+@st.cache_resource
+def read_data(problem: str, str_model: str):
+
+    data = loadmat(f"data/fhn_trainL2_n_375_points_1260_tf_100_{str_model}.mat")
+    match problem:
+        case "darcy":
+            data = loadmat(f"data/fhn_trainL2_n_375_points_1260_tf_100_{str_model}.mat")
+        case "HH":
+            data = loadmat(f"data/hh_trainL2_n_375_points_1260_tf_100_{str_model}.mat")
+        case "ORD":
+            data = loadmat(
+                f"data/ord_trainL2_n_375_points_2520_tf_1000_{str_model}.mat"
+            )
+        case _:
+            raise ValueError(f"Invalid problem: {problem}")
+
+    return data
+
+
+@st.cache_data
+def plot_input(
+    problem: str, str_model: str, attribute: str, sample_idx: int, ylabel: str = None
+):
+    """
+    problem: str
+        The name of the problem selected by the user.
+
+    str_model: str
+        The name of the model selected by the user (best, best_samedofs).
+
+    attribute: str
+        The attribute of the model to be plotted.
+
+    sample_idx: int
+        The index of the sample to be plotted.
+
+    index_plot: int
+        The index of the plot to be displayed.
+
+    ylabel: str
+        The label of the y-axis.
+    """
+
+    data = read_data(problem, str_model)
+
+    y_data = data[attribute][sample_idx]
+    if attribute == "input":
+        y_data = np.round(y_data, 4)
+
+    if problem == "FHN" or problem == "HH":
+        x_data = np.linspace(0, 100, len(y_data))
+    else:
+        x_data = np.linspace(0, 1000, len(y_data))
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=x_data,
+            y=y_data,
+            mode="lines",
+        )
+    )
+
+    if ylabel:
+        fig.update_layout(
+            yaxis_title=ylabel,
+        )
+
+    fig.update_layout(
+        xaxis_title="Time (ms)",
+        font=dict(family="Arial", size=12),
+        # plot_bgcolor="#f5f7fa",
+        # paper_bgcolor="#ffffff",
+        width=400,
+        height=300,
+        margin=dict(
+            l=20,  # left margin
+            r=20,  # right margin
+            t=30,  # top margin
+            b=20,  # bottom margin
+        ),
+    )
+
+    return fig
+
+
+@st.cache_data
+def plot_outputs(
+    problem: str, str_model: str, attribute: str, sample_idx: int, ylabel: str = None
+):
+    """
+    Same as plot_input, but with the plot of two samples overlapped.
+    """
+
+    data = read_data(problem, str_model)
+
+    y_data_exact = data[f"{attribute}_exact"][sample_idx]
+    y_data_appro = data[f"{attribute}_pred"][sample_idx]
+
+    if problem == "FHN" or problem == "HH":
+        x_data = np.linspace(0, 100, len(y_data_exact))
+    else:
+        x_data = np.linspace(0, 1000, len(y_data_exact))
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=x_data,
+            y=y_data_appro,
+            mode="lines",
+            line=dict(color="#9649cb"),
+            name="FNO",
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=x_data,
+            y=y_data_exact,
+            mode="lines",
+            line=dict(color="#f35b04"),
+            name="Exact",
+        )
+    )
+
+    if ylabel:
+        fig.update_layout(
+            yaxis_title=ylabel,
+        )
+
+    fig.update_layout(
+        xaxis_title="Time (ms)",
+        font=dict(family="Arial", size=12),
+        # plot_bgcolor="#f5f7fa",
+        # paper_bgcolor="#ffffff",
+        width=400,
+        height=300,
+        margin=dict(
+            l=20,  # left margin
+            r=20,  # right margin
+            t=55,  # top margin
+            b=20,  # bottom margin
+        ),
+        legend=dict(x=0.5, y=1.0, xanchor="center", yanchor="bottom", orientation="h"),
+    )
+
+    return fig
+
+
+@st.cache_data
+def plot_errors(
+    problem: str, str_model: str, attribute: str, sample_idx: int, ylabel: str = None
+):
+    """
+    Same as plot_input, but with the plot of the errors between the exact solution and the approximation.
+    """
+
+    data = read_data(problem, str_model)
+
+    y_data_exact = data[f"{attribute}_exact"][sample_idx]
+    y_data_appro = data[f"{attribute}_pred"][sample_idx]
+
+    if problem == "FHN" or problem == "HH":
+        x_data = np.linspace(0, 100, len(y_data_exact))
+    else:
+        x_data = np.linspace(0, 1000, len(y_data_exact))
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=x_data,
+            y=np.abs(y_data_appro - y_data_exact),
+            mode="lines",
+            name="error",
+        )
+    )
+
+    if ylabel:
+        fig.update_layout(
+            yaxis_title=ylabel,
+        )
+
+    fig.update_yaxes(type="log")
+
+    fig.update_layout(
+        xaxis_title="Time (ms)",
+        font=dict(family="Arial", size=12),
+        # plot_bgcolor="#f5f7fa",
+        # paper_bgcolor="#ffffff",
+        width=400,
+        height=300,
+        margin=dict(
+            l=20,  # left margin
+            r=20,  # right margin
+            t=30,  # top margin
+            b=20,  # bottom margin
+        ),
+    )
+
+    return fig
